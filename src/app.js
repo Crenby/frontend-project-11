a2be24b96e75b92ffaf6c64f6e80bb828f997288
Crenby <arkadiy.shtolts@mail.ru>
2024-08-12 13:35:29 +0300

    step 4

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import onChange from 'on-change';
 import view from './view';
 import i18next from 'i18next';
 import resources from './locales/index.js';
+import axios from 'axios';
+import parser from './parser.js';
 
 export default function app() {
 
@@ -13,6 +15,7 @@ export default function app() {
       errors: null,
     },
     fids: [],
+    data: [],
   };
 
   const defaultLang = 'ru';
@@ -27,16 +30,15 @@ export default function app() {
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('#url-input'),
-    errorFields: {},
   }
 
   yup.setLocale({
     mixed: {
       required: i18n.t('errors.required'),
-      notOneOf:  i18n.t('errors.exists'),
+      notOneOf: i18n.t('errors.exists'),
     },
     string: {
-      url:  i18n.t('errors.invalidUrl'),
+      url: i18n.t('errors.invalidUrl'),
     },
   })
 
@@ -46,9 +48,9 @@ export default function app() {
     });
     return schema.validate(fields);
   };
- 
+
   const watchedState = onChange(state, (path, value) => {
-    view(path, value);
+    view(state, path, value);
   });
 
   elements.form.addEventListener('submit', (e) => {
@@ -57,21 +59,36 @@ export default function app() {
     const formData = new FormData(e.target);
     const out = formData.get('url');
 
-    validate({url: out}, state.fids)
+    validate({ url: out }, state.fids)
       .then(() => {
         console.log("good");
         watchedState.fids.push(out);
         watchedState.form.validUrl = false;
         watchedState.form.errors = null;
+
+        axios.get(`https://allorigins.hexlet.app/get?url=${out}`)
+          .then((response) => {
+            watchedState.data.unshift(parser(response)); 
+            watchedState.form.status = i18n.t('errors.validUrl');   
+          })
+          .catch((e) => {
+            if(e.isParsingError) {
+              watchedState.form.errors = i18n.t('errors.invalidRss');
+            } else if(e.message === 'Network Error') {
+              watchedState.form.errors = i18n.t('errors.network');
+            } else {
+              watchedState.form.errors = i18n.t('errors.unknown');
+            }
+          })
+
         elements.input.focus();
         elements.form.reset();
       })
       .catch((e) => {
         console.log("nogood");
-        console.log(e.message);
+        watchedState.form.status = null;
         watchedState.form.validUrl = true;
         watchedState.form.errors = e.message;
       })
-
   });
 }
--- src/view.js
+++ src/view.js
@@ -1,10 +1,80 @@
 const input = document.querySelector("#url-input");
 const feedback = document.querySelector(".feedback");
+const posts = document.querySelector(".posts");
+const feeds = document.querySelector(".feeds");
 
-export default function view(path, value) {
-  switch(path) {
+function renderFeeds(data) {
+  feeds.innerHTML = '';
+
+  const div = document.createElement('div');
+  div.classList.add('card', 'border-0');
+
+  const insDiv = document.createElement('div');
+  insDiv.classList.add('card-body');
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('card-title', 'h4');
+  h2.textContent = 'Фиды';
+  insDiv.append(h2);
+
+  div.append(insDiv);
+
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+
+  data.forEach((item) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    li.innerHTML = `
+      <h3 class="h6 m-0">${item.title}</h3>
+      <p class="m-0 small text-black-50">${item.description}</p>
+    `;
+    ul.append(li);
+  })
+
+  div.append(ul);
+  feeds.append(div);
+}
+
+function renderPosts(data) {
+  posts.innerHTML = '';
+  const div = document.createElement('div');
+  div.classList.add('card', 'border-0');
+
+  const insDiv = document.createElement('div');
+  insDiv.classList.add('card-body');
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('card-title', 'h4');
+  h2.textContent = 'Посты';
+  insDiv.append(h2);
+
+  div.append(insDiv);
+
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+
+  data.forEach((item) => {
+    item.items.forEach((post) => {
+      const li = document.createElement('li');
+      li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+
+      li.innerHTML = `
+        <a href="${post.link}" class="fw-bold" data-id="137" target="_blank" rel="noopener noreferrer">${post.title}</a>
+        <button type="button" class="btn btn-outline-primary btn-sm" data-id="137" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>
+      `;
+      ul.append(li);
+    })
+  })
+
+  div.append(ul);
+  posts.append(div);
+}
+
+export default function view(state, path, value) {
+  switch (path) {
     case 'form.validUrl':
-      if(value) {
+      if (value) {
         input.classList.add('is-invalid');
       } else {
         input.classList.remove('is-invalid');
@@ -12,5 +82,18 @@ export default function view(path, value) {
       break;
     case 'form.errors':
       feedback.textContent = value;
+      feedback.classList.add('text-danger');
+      feedback.classList.remove('text-success');
+      break;
+    case 'form.status':
+      feedback.textContent = value;
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
+      break;
+    case 'data':
+      console.log(state.data);
+      renderFeeds(state.data);
+      renderPosts(state.data);
+      break;
   }
 }
